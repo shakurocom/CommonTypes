e8d34c29b94eb8fe13235055b10b2543cb9de6e4
Sergey <wwwpix@ya.com>
2024-02-01 14:57:24 +0300

    II-16-[fixed keyboard frame issue when reduce motion accessibility setting is enabled]

--- Source/UICollectionView+SafeUpdate.swift
+++ Source/UICollectionView+SafeUpdate.swift
@@ -7,12 +7,13 @@ import UIKit
 
 extension UICollectionView {
 
-    public func safePerformBatchUpdates(_ updates: (() -> Void)?, completion: ((Bool) -> Void)?) {
+    public func safePerformBatchUpdates(_ updates: (() -> Void)?, completion: ((Bool) -> Void)? = nil) {
         if window != nil {
             performBatchUpdates(updates, completion: completion)
         } else {
             // collection view will crash on performBatchUpdates when window == nil
             reloadData()
+            completion?(true)
         }
     }
 
