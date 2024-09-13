4c16cc00cb4ab618d9f4a09dea09d13dfa8a2547
Sanaka <slaschuk@shakuro.com>
2024-09-13 17:47:03 +0300

    II-16-[added method to manually simulate deceleration]

--- Source/DecelerationHelper.swift
+++ Source/DecelerationHelper.swift
@@ -8,6 +8,7 @@ import UIKit
 public struct DecelerationHelper {
 
     /// - parameter initialVelocity: in points/millisecond
+    /// - parameter decelerationRate: (0; 1)
     public static func project(value: CGFloat, initialVelocity: CGFloat, decelerationRate: CGFloat) -> CGFloat {
         guard decelerationRate < 1.0 else {
             assert(false)
@@ -26,6 +27,20 @@ public struct DecelerationHelper {
     /// - parameter initialVelocity: in points/millisecond
     public static func project(value: CGPoint, initialVelocity: CGPoint, decelerationRate: CGFloat) -> CGPoint {
         return project(value: value, initialVelocity: initialVelocity, decelerationRate: CGPoint(x: decelerationRate, y: decelerationRate))
+    }
+
+    /// - parameter initialVelocity: in points/millisecond
+    /// - parameter decelerationRate: rate of velocity dropping. See `UIScrollView.DecelerationRate`. Usually [0.99 - 0.998].
+    /// - parameter precisionThreshold: Precision of projected value. Because mathematically deceleration will never stop.
+    ///
+    /// https://medium.com/@esskeetit/scrolling-mechanics-of-uiscrollview-142adee1142c
+    public static func duration(initialVelocity: CGFloat, decelerationRate: CGFloat, precisionThreshold: CGFloat) -> TimeInterval {
+        assert(decelerationRate < 1 && decelerationRate > 0)
+        guard abs(initialVelocity) > 0 else {
+            return 0
+        }
+        let dCoeff = 1000 * log(decelerationRate)
+        return TimeInterval(log(-dCoeff * precisionThreshold / abs(initialVelocity)) / dCoeff)
     }
 
 }
